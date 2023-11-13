f15c80cc016c32699db34800506fc7d8b7f86a59
Zach <zdgemmill@gmail.com>
2023-11-13 12:37:45 -0700

    Added end game function, fixes to bugs

--- assets/script.js
+++ assets/script.js
@@ -74,26 +74,33 @@ startButtonEl.addEventListener("click", function (e) {
 var timeLeft = 30;
 function countdown() {
     var timeInterval = setInterval(function () {
-        if (timeLeft > 1) {
-            timerEl.textContent = timeLeft;
+        if (timeLeft > 0) {
             timeLeft--;
+            timerEl.textContent = timeLeft;
+        } else if (timeLeft == -50) {
+            timerEl.textContent = '';
+            clearInterval(timeInterval);
+
         } else {
             timerEl.textContent = '';
             clearInterval(timeInterval);
+            endGame();
             // Needs to be taken to prompt to enter score. 
         }
     }, 1000);
 }
 
 
-//shows first question 
+//function for showing first question and answers
 function showQuestion() {
     // Clear existing buttons
     button.innerHTML = '';
-
+    if (currentQuestionI > questions.length) {
+        return
+    };
     var currentQuestion = questions[currentQuestionI];
     questionEl.innerHTML = currentQuestion.question;
-
+    //this is creating my button elements and appending them to the buttons div as child emelents 
     currentQuestion.answers.forEach(answer => {
         var answerButton = document.createElement("button");
         answerButton.innerHTML = answer.text;
@@ -104,17 +111,15 @@ function showQuestion() {
         answerButton.addEventListener("click", function (e) {
             checkAnswer(e.target);
         });
+
     });
 }
 var currentQuestionI = 0;
 
-
+//my function for checking if the user selected answer is the correct answer. If so do something with the time. show correct message when answer is selected
 function checkAnswer(selectedButton) {
     var selectedAnswerText = selectedButton.innerText;
     var correctAnswerText = getCurrentQuestionCorrectAnswer();
-
-    console.log("Selected Answer: " + selectedAnswerText);
-    console.log("Correct Answer: " + correctAnswerText);
 
     if (selectedAnswerText === correctAnswerText) {
         timeLeft += 10;
@@ -129,11 +134,12 @@ function checkAnswer(selectedButton) {
     if (currentQuestionI < questions.length) {
         showQuestion();
     } else {
-        console.log("End of the quiz!");
+        timeLeft = -50;
+        endGame();
         // show the user's score and ask for their initials
     }
 }
-
+//this function is getting the correct answer for the current question 
 function getCurrentQuestionCorrectAnswer() {
     var currentQuestion = questions[currentQuestionI];
     // Correctly find the correct answer text
@@ -141,4 +147,14 @@ function getCurrentQuestionCorrectAnswer() {
     return correctAnswerText;
 }
 
-//needs to create an answer checker some how
+
+//this is for endgame
+var intialsForm = document.createElement("form");
+
+function endGame() {
+    questionEl.innerHTML = "You scored a " + userScore + "."
+    button.innerHTML = "";
+    msgEl.innerHTML = "";
+
+
+};
